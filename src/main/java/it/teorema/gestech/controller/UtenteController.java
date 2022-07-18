1ae15d5c00d6311f71aa2187a8642af83567c2ba
LeoNellus8915 <leonellus8915@gmail.com>
2022-07-18 14:12:26 +0200

    Aggiungiutente

--- src/main/java/it/teorema/gestech/controller/UtenteController.java
+++ src/main/java/it/teorema/gestech/controller/UtenteController.java
@@ -1,8 +1,31 @@
 package it.teorema.gestech.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UtenteController {
-
+	@RequestMapping("/nuovoUtente")
+	public String nuovoUtente(HttpServletRequest request, Model theModel) {
+		/**
+		 * passo il nome della pagina che si deve caricare
+		 * i file si trovano in template/subPage
+		 */
+		HttpSession session = request.getSession(true);
+		String nome_cognome = (String) session.getAttribute("nome_cognome");
+		String ruolo = (String) session.getAttribute("ruolo");
+		
+		theModel.addAttribute("nome_cognome", nome_cognome);
+		theModel.addAttribute("ruolo", ruolo);
+		theModel.addAttribute("titlePage", "Utente");
+		theModel.addAttribute("view", "nuovoUtente");
+		/**
+		 * nome del tempate da usare (default/login)
+		 */
+		return "default";
+	}
 }
