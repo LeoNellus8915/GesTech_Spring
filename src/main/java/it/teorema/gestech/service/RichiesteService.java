ed1c3323e8e0ac497ceed160d5f2012a9efe12bc
Jack-Gramorn <dioguardigiacomo96@gmail.com>
2022-07-29 16:52:23 +0200

    jh

--- src/main/java/it/teorema/gestech/service/RichiesteService.java
+++ src/main/java/it/teorema/gestech/service/RichiesteService.java
@@ -19,11 +19,9 @@ public interface RichiesteService extends JpaRepository <Richieste, Integer>
 	abstract
 	Richieste findById(int idRichiesta);
 
-	@Query("select ri.id, ri.data, ri.cliente, ri.citta, ri.costo, ri.note, li.nome as nome_linguaggio, "
-			+ "pro.nome as nome_profilo, liv.nome as nome_seniority, sr.nome as nome_stato "
-			+ "from hy_richieste ri, ty_linguaggi li, ty_profili pro, ty_livelli liv, ty_stati_richiesta sr "
-			+ "where ri.id_seniority = :idSeniority and ri.id_skill = :idSkill and "
-			+ "ri.id_profilo = :idProfilo and ri.id_stato = :idStato")
+	@Query("select ri.id, ri.data, ri.cliente, ri.citta, ri.costo, ri.note, li.nome as nome_linguaggio, pro.nome as nome_profilo, liv.nome as nome_seniority, sr.nome as nome_stato "
+			+ "from Richieste ri, Linguaggi li, Profili pro, Livelli liv, StatiRichiesta sr "
+			+ "where ri.idSeniority = :idSeniority and ri.idSkill = :idSkill and ri.idProfilo = :idProfilo and ri.idStato = :idStato")
 	abstract 
 	List stampaCard(int idSeniority, int idSkill, int idProfilo, int idStato);
 }
